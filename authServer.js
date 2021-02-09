2b89de4344b1b77d6f9b39118fbd5c1206cefe7c
MatheusXimenes <matheus.n.ximenes@gmail.com>
2021-02-08 20:02:24 -0500

    Work in Progress 2

--- authServer.js
+++ authServer.js
@@ -5,29 +5,54 @@ const jwt = require("jsonwebtoken");
 const app = express();
 app.use(express.json());
 
-app.post("/auth", (req, res) => {
+//TODO: Move that Concept to a Database
+let refreshTokens = [];
+
+app.post("/token", (req, res) => {
+  const refreshToken = req.body.token;
+  if (refreshToken === null) {
+    return res.sendStatus(401);
+  }
+
+  if (!refreshToken.includes(refreshToken)) {
+    return res.sendStatus(403);
+  }
+
+  jwt.verify(refreshToken, progress.env.ACCESS_TOKEN_REFRESH, (err, user) => {
+    if (err) {
+      return res.sendStatus(403);
+    }
+    const accessToken = GenerateJWTToken({ name: user.name });
+    res.json({ accessToken: accessToken });
+  });
+});
+
+app.post("/login", (req, res) => {
   const user = {
     name: req.body.name,
   };
-  const token = jwt.sign(user, process.env.ACCESS_TOKEN_PUBLIC);
-  res.json({ accessToken: token });
+  const token = GenerateJWTToken(user, false);
+  const refreshToken = GenerateJWTToken(user, true);
+  refreshTokens.push(refreshToken);
+  res.json({ accessToken: token, refreshToken: refreshToken });
 });
 
-export function authenticateToken(req, res, next) {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-  if (token === null) {
-    return res.sendStatus(401);
-  } else {
-    jwt.verify(token, process.env.ACCESS_TOKEN_PUBLIC, (err, user) => {
-      if (err) {
-        return res.sendStatus(403);
-      } else {
-        req.user = user;
-        next();
-      }
-    });
-  }
-}
+app.delete("/logout", (req, res) => {
+  refreshTokens = refreshTokens.filter((t) => t !== req.body.token);
+});
+
+const GenerateJWTToken = (user, useRefresh) => {
+  return jwt.sign(
+    user,
+    useRefresh
+      ? process.env.ACCESS_TOKEN_REFRESH
+      : process.env.ACCESS_TOKEN_PUBLIC,
+    !useRefresh
+      ? {
+          expiresIn: "30s",
+        }
+      : {}
+  );
+};
 
 app.listen(4000);
--- server.js
+++ server.js
@@ -16,8 +16,23 @@ const posts = [
   },
 ];
 
-app.get("/post", authenticateToken, (req, res) => {
+app.get("/post", AuthenticateToken, (req, res) => {
   res.json(posts.filter((p) => p.userName === req.user.name));
 });
+
+function AuthenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token === null) {
+    return res.sendStatus(401);
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_PUBLIC, (err, user) => {
+    if (err) {
+      return res.sendStatus(403);
+    }
+    req.user = user;
+    next();
+  });
+}
 
 app.listen(3000);
